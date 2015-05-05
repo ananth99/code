35881bc8458facf31a402c9f1d1309452e3f5406
Ananth Madhavan <ananthmadhavan6@gmail.com>
2015-05-05 11:11:15 +0530

    custom map method ruby

---
+++ custom_enumerable.rb
@@ -0,0 +1,10 @@
+# Custom enumerable module
+module CustomEnumerable
+  def map(&block)
+    result = []
+    each do |element|
+      result << block.call(element)
+    end
+    result 
+  end
+end
