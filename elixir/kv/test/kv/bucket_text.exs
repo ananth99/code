816a09032fa13bc1ad57cb771663723aa8adb922
Ananth Madhavan <ananthmadhavan6@gmail.com>
2016-01-11 01:41:54 +0530

    use ExUnit callback setup to start KV module

--- elixir/kv/test/kv/bucket_text.exs
+++ elixir/kv/test/kv/bucket_text.exs
@@ -1,8 +1,12 @@
 defmodule Kv.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
-    {:ok, bucket} =Kv.Bucket.start_link
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
     assert Kv.Bucket.get(bucket, "milk") == nil
 
     Kv.Bucket.put(bucket, "milk", 3)
