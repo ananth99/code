27693446778f0b27a68bfeb0dad196d70756c741
Ananth Madhavan <ananthmadhavan6@gmail.com>
2016-02-24 00:34:32 +0530

    remove process monit from code repo

--- elixir/process_monit/lib/process_monit.ex
+++
@@ -1,3 +0,0 @@
-defmodule ProcessMonit do
-  ## Client abstractions to start/stop monitoring processes go here.
-end
--- elixir/process_monit/test/process_monit_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ProcessMonitTest do
-  use ExUnit.Case, async: true
-  doctest ProcessMonit
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
